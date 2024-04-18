8255b3532cd444c711c70eedf244f8e7c7ba5354
Lambda-Analytica <164220003+Lambda-Analytica@users.noreply.github.com>
2024-04-18 11:31:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ server = app.server
 #========================================LOAD DATA========================================
 
 def load_data_from_gcs(blob_name):
-    bucket_name = os.getenv('dinex_bucket')  # Retrieve the bucket name from environment variables
+    bucket_name = os.getenv('BUCKET_NAME')  # Corrected to the right environment variable
     try:
         client = storage.Client()
         bucket = client.bucket(bucket_name)
